20d1d90c0b3d5a57887c2836bf91ede0b7d54e8e
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-04 12:07:19 +0100

    stylich fixes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,12 @@
 <script len="ts" setup>
-  const {data: tasks, error, status} = await useFetch('/api/tasks');
+const { data: tasks, error, status } = await useFetch("/api/tasks");
 </script>
 
-
 <template>
-  <article v-for="task in tasks" :key="task.id">
-      {{ task.title }}
-  </article>
+	<article
+		v-for="task in tasks"
+		:key="task.id"
+	>
+		{{ task.title }}
+	</article>
 </template>
