8b8cc7d9ff4a945a4bf37c9fb5b2261eafed2e08
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-04 11:49:19 +0100

    styling error fixed

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,14 +1,14 @@
 <template>
-<nav class="container">
-        <ul>
-            <li>
-                <nuxt-link to="/">
-                    <strong>Tasks App</strong>
-                </nuxt-link>
-            </li>
-        </ul>
-        <ul>
-            <li><NuxtLink :to="{ name: 'create' }">create</NuxtLink></li>
-        </ul>
-    </nav>
+	<nav class="container">
+		<ul>
+			<li>
+				<nuxt-link to="/">
+					<strong>Tasks App</strong>
+				</nuxt-link>
+			</li>
+		</ul>
+		<ul>
+			<li><NuxtLink :to="{ name: 'create' }">create</NuxtLink></li>
+		</ul>
+	</nav>
 </template>
